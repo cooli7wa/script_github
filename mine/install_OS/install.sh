ba9b35598f0f456c6683e794265bbb7245e58f14
liuqi <liuqi@microtrust.com.cn>
2017-12-29 14:53:38 +0800

    update install os
    Issue No:

--- mine/install_OS/install.sh
+++ mine/install_OS/install.sh
@@ -26,15 +26,15 @@ function echo_color
 }
 
 
-packages="vim zlib1g-dev:i386 g++-multilib ssh make gawk g++ binutils pkg-config synaptic terminator ant u-boot-tools gperf bison doxygen libswitch-perl libxml2-utils libssl-dev xsel adb git sshfs"
+packages="vim zlib1g-dev:i386 g++-multilib ssh make gawk g++ binutils pkg-config synaptic terminator ant u-boot-tools gperf bison doxygen libswitch-perl libxml2-utils libssl-dev xsel adb git sshfs repo"
 
 echo_color RED "apt-get update .."
 sudo apt-get update
 
 echo_color RED "apt-get install .."
-sudo apt-get install $packages
+sudo apt-get install -y $packages
 
-echo_color RED "install openjdk-8 .."
+echo_color RED "install openjdk-8 .. (1604 don't need)"
 
 echo_color RED "install chrome .."
 sudo dpkg -i google-chrome-stable_current_amd64.deb
@@ -43,4 +43,11 @@ echo_color RED "install sogou .."
 sudo dpkg -i sogoupinyin_2.2.0.0102_amd64.deb
 echo_color YELLOW "please config next: \"im-config\" \"fcitx-config-gtk3\""
 
+echo_color RED "install needed libraries before.."
+sudo apt-get install -f -y
+
 echo_color RED "config bashrc inputrc vimrc .."
+
+echo_color RED "generate sshkey .."
+sudo ssh-keygen
+cat ~/.ssh/id_rsa.pub
