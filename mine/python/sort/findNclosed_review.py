95566682a405a64cf854c4d2cbd1beeba61ccafa
cooli7wa <liuqi@microtrust.com.cn>
2018-05-08 09:49:42 +0800

    a

---
+++ mine/python/sort/findNclosed_review.py
@@ -0,0 +1,13 @@
+import sys
+
+class Solution:
+    def threeClosed(self, nums, target):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        pass
+
+
+s = Solution()
+print(s.threeClosed([-1,0,1,1,55], 3))
---
+++ mine/python/sort/findNsum_review.py
@@ -0,0 +1,11 @@
+class Solution:
+    def threeSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        pass
+
+
+s = Solution()
+print(s.threeSum([1, 0, -1, 0, -2, 2], 0))
